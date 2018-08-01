d961a9164ac824023f254a891529b430ec4d3309
PiotrSroda <Piotr.w.sroda@gmail.com>
2018-08-01 18:28:36 +0200

    added background video and viniettes

--- src/App.js
+++ src/App.js
@@ -5,97 +5,101 @@ import './fonts/icon-font.css';
 import Photo_1 from './img/nat-1-large.jpg';
 import Photo_2 from './img/nat-2-large.jpg';
 import Photo_3 from './img/nat-3-large.jpg';
+import figure1 from './img/nat-8.jpg';
+import figure2 from './img/nat-9.jpg';
+import video from './img/video.mp4';
+import video1 from './img/video.webm';
 
 
 class App extends Component {
   render() {
     return (
     <div>
-      <header class='header'>
-        <div class='header__logo-box'>
-          <img src = {logo} alt='Logo' class='header__logo'/>
+      <header className='header'>
+        <div className='header__logo-box'>
+          <img src = {logo} alt='Logo' className='header__logo'/>
         </div>
-        <div class='header__text-box'>
-          <h1 class='heading-primary'>
-            <span class='heading-primary--main'>Outdoors</span>
-            <span class='heading-primary--sub'>is where life happens</span>
+        <div className='header__text-box'>
+          <h1 className='heading-primary'>
+            <span className='heading-primary--main'>Outdoors</span>
+            <span className='heading-primary--sub'>is where life happens</span>
           </h1>
 
-          <a href='#' class='btn btn--white btn--animated'>Discover our tours</a>
+          <a href='#' className='btn btn--white btn--animated'>Discover our tours</a>
         </div>
         </header>
 
         <main>
-            <section class= 'section-about'>
-              <div class='u-center-text u-margin-bottom-big'> {/*UTILITY CLASSES*/}
-                <h2 class= 'heading-secondary'>
+            <section className= 'section-about'>
+              <div className='u-center-text u-margin-bottom-big'> {/*UTILITY classNameES*/}
+                <h2 className= 'heading-secondary'>
                 Exciting tours for adventurous people
                 </h2>
               </div>
 
-                <div class='row'>
-                  <div class='col-1-of-2'>
+                <div className='row'>
+                  <div className='col-1-of-2'>
                     <h3 className="heading-tertiary u-margin-bottom-small"> You're going to fall in love with nature</h3>
-                    <p class="paragraph">
+                    <p className="paragraph">
                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
 
                     <h3 className="heading-tertiary u-margin-bottom-small"> Live adventures like you never have before</h3>
-                    <p class="paragraph">
+                    <p className="paragraph">
                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     </p>
 
-                    <a href="#" class="btn-text">Learn more &rarr;</a>
+                    <a href="#" className="btn-text">Learn more &rarr;</a>
                   </div>
-                  <div class='col-1-of-2'>
-                    <div class="composition">
-                      <img src= {Photo_1} alt="P1" class="composition__photo composition__photo--p1"/>
-                      <img src= {Photo_2} alt="P2" class="composition__photo composition__photo--p2"/>
-                      <img src= {Photo_3} alt="P3" class="composition__photo composition__photo--p3"/>
+                  <div className='col-1-of-2'>
+                    <div className="composition">
+                      <img src= {Photo_1} alt="P1" className="composition__photo composition__photo--p1"/>
+                      <img src= {Photo_2} alt="P2" className="composition__photo composition__photo--p2"/>
+                      <img src= {Photo_3} alt="P3" className="composition__photo composition__photo--p3"/>
                     </div>
                   </div>
                 </div>
             </section>
 
-            <section class="section-features">
+            <section className="section-features">
 
 
-                <div class="row">
-                  <div class="col-1-of-4">
-                    <div class="feature-box">
-                        <i class="feature-box__icon icon-basic-world"></i>
-                        <h3 class="heading-tertiary u-margin-bottom-small">Explore the world</h3>
-                        <p class="feature-box__text">
+                <div className="row">
+                  <div className="col-1-of-4">
+                    <div className="feature-box">
+                        <i className="feature-box__icon icon-basic-world"></i>
+                        <h3 className="heading-tertiary u-margin-bottom-small">Explore the world</h3>
+                        <p className="feature-box__text">
                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                     </div>
                   </div>
 
-                  <div class="col-1-of-4">
-                    <div class="feature-box">
-                        <i class="feature-box__icon icon-basic-compass"></i>
-                        <h3 class="heading-tertiary u-margin-bottom-small">Meet nature</h3>
-                        <p class="feature-box__text">
+                  <div className="col-1-of-4">
+                    <div className="feature-box">
+                        <i className="feature-box__icon icon-basic-compass"></i>
+                        <h3 className="heading-tertiary u-margin-bottom-small">Meet nature</h3>
+                        <p className="feature-box__text">
                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                     </div>
                   </div>
 
-                  <div class="col-1-of-4">
-                    <div class="feature-box">
-                        <i class="feature-box__icon icon-basic-map"></i>
-                        <h3 class="heading-tertiary u-margin-bottom-small">Find Your way</h3>
-                        <p class="feature-box__text">
+                  <div className="col-1-of-4">
+                    <div className="feature-box">
+                        <i className="feature-box__icon icon-basic-map"></i>
+                        <h3 className="heading-tertiary u-margin-bottom-small">Find Your way</h3>
+                        <p className="feature-box__text">
                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                     </div>
                   </div>
 
-                  <div class="col-1-of-4">
-                    <div class="feature-box">
-                        <i class="feature-box__icon icon-basic-heart"></i>
-                        <h3 class="heading-tertiary u-margin-bottom-small">Live a healthier life</h3>
-                        <p class="feature-box__text">
+                  <div className="col-1-of-4">
+                    <div className="feature-box">
+                        <i className="feature-box__icon icon-basic-heart"></i>
+                        <h3 className="heading-tertiary u-margin-bottom-small">Live a healthier life</h3>
+                        <p className="feature-box__text">
                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                     </div>
@@ -103,17 +107,17 @@ class App extends Component {
                 </div>
             </section>
 
-            <section class="section-tours">
-              <div class='u-center-text u-margin-bottom-big'> {/*UTILITY CLASSES*/}
-                <h2 class= 'heading-secondary'>
+            <section className="section-tours">
+              <div className='u-center-text u-margin-bottom-big'> {/*UTILITY classNameES*/}
+                <h2 className= 'heading-secondary'>
                   Most popular tours
                 </h2>
               </div>
 
-              <div class='row'>
-                <div class='col-1-of-3'>
-                  <div class="card">
-                    <div class="card__side card__side--front">
+              <div className='row'>
+                <div className='col-1-of-3'>
+                  <div className="card">
+                    <div className="card__side card__side--front">
                       <div className="card__picture card__picture--1">
                         &nbsp;
                       </div>
@@ -130,20 +134,20 @@ class App extends Component {
                         </ul>
                       </div>
                     </div>
-                    <div class="card__side card__side--back card__side--back-1">
-                      <div class="card__cta">
-                          <div class="card__price-box">
-                              <p class="card__price-only">Only</p>
-                              <p class="card__price-value">$297</p>
+                    <div className="card__side card__side--back card__side--back-1">
+                      <div className="card__cta">
+                          <div className="card__price-box">
+                              <p className="card__price-only">Only</p>
+                              <p className="card__price-value">$297</p>
                           </div>
-                          <a href="" class="btn btn--white">Book now!</a>
+                          <a href="" className="btn btn--white">Book now!</a>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class='col-1-of-3'>
-                  <div class="card">
-                    <div class="card__side card__side--front">
+                <div className='col-1-of-3'>
+                  <div className="card">
+                    <div className="card__side card__side--front">
                       <div className="card__picture card__picture--2">
                         &nbsp;
                       </div>
@@ -160,20 +164,20 @@ class App extends Component {
                         </ul>
                       </div>
                     </div>
-                    <div class="card__side card__side--back card__side--back-2">
-                      <div class="card__cta">
-                          <div class="card__price-box">
-                              <p class="card__price-only">Only</p>
-                              <p class="card__price-value">$585</p>
+                    <div className="card__side card__side--back card__side--back-2">
+                      <div className="card__cta">
+                          <div className="card__price-box">
+                              <p className="card__price-only">Only</p>
+                              <p className="card__price-value">$585</p>
                           </div>
-                          <a href="" class="btn btn--white">Book now!</a>
+                          <a href="" className="btn btn--white">Book now!</a>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class='col-1-of-3'>
-                  <div class="card">
-                    <div class="card__side card__side--front">
+                <div className='col-1-of-3'>
+                  <div className="card">
+                    <div className="card__side card__side--front">
                       <div className="card__picture card__picture--3">
                         &nbsp;
                       </div>
@@ -190,99 +194,140 @@ class App extends Component {
                         </ul>
                       </div>
                     </div>
-                    <div class="card__side card__side--back card__side--back-3">
-                      <div class="card__cta">
-                          <div class="card__price-box">
-                              <p class="card__price-only">Only</p>
-                              <p class="card__price-value">$767</p>
+                    <div className="card__side card__side--back card__side--back-3">
+                      <div className="card__cta">
+                          <div className="card__price-box">
+                              <p className="card__price-only">Only</p>
+                              <p className="card__price-value">$767</p>
                           </div>
-                          <a href="" class="btn btn--white">Book now!</a>
+                          <a href="" className="btn btn--white">Book now!</a>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
 
-              <div class='u-center-text u-margin-top-huge'>
-                <a href="#" class="btn btn--green">Discover all tours</a>
+              <div className='u-center-text u-margin-top-huge'>
+                <a href="#" className="btn btn--green">Discover all tours</a>
               </div>
 
             </section>
 
-            <section class="section-stories">
-                <div class="u-center-text umargin-bottom-big">
-                  <h2 class="heading-secondary">
-                    Most Popular tours
-                  </h2>
-                </div>
+            <section className="section-stories">
 
+            <div className="bg-video">
+              <video className="bg-video__content" autoPlay muted loop>
+                <source src={video} type='video/mp4'/>
+                <source src={video1} type='video/webm'/>
+                Your browser is not supported
+              </video>
+            </div>
 
+              <div className='u-center-text u-margin-bottom-big'> {/*UTILITY classNameES*/}
+                <h2 className= 'heading-secondary'>
+                  We make people genuinely happy
+                </h2>
+              </div>
+
+              <div className="row">
+                      <div className="story">
+                          <figure className="story__shape">
+                          <img src={figure1} alt="Person on a tour" className="story__img"/>
+                          <figcaption className="story__caption">Mary Smith</figcaption>
+                          </figure>
+                          <div className="story__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">I had the best week ever with my family</h3>
+                            <p>
+                              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+                            </p>
+                          </div>
+                      </div>
+              </div>
+
+              <div className="row">
+                      <div className="story">
+                          <figure className="story__shape">
+                          <img src={figure2} alt="Person on a tour" className="story__img"/>
+                          <figcaption className="story__caption">Jack Wilson</figcaption>
+                          </figure>
+                          <div className="story__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">WOW! My life is completely diffent now </h3>
+                            <p>
+                              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+                            </p>
+                          </div>
+                      </div>
+              </div>
+
+              <div className='u-center-text u-margin-top-huge'>
+                <a href="#" className="btn-text">Read all stories &rarr;</a>
+              </div>
             </section>
         </main>
 {/*
-        <section class='grid-test'>
-          <div class='row'>
-            <div class='col-1-of-2'>
+        <section className='grid-test'>
+          <div className='row'>
+            <div className='col-1-of-2'>
               Col 1 of 2
             </div>
-            <div class='col-1-of-2'>
+            <div className='col-1-of-2'>
               Col 1 of 2
             </div>
           </div>
 
-          <div class='row'>
-            <div class='col-1-of-3'>
+          <div className='row'>
+            <div className='col-1-of-3'>
               Col 1 of 3
             </div>
-            <div class='col-1-of-3'>
+            <div className='col-1-of-3'>
               Col 1 of 3
             </div>
-            <div class='col-1-of-3'>
+            <div className='col-1-of-3'>
               Col 1 of 3
             </div>
           </div>
 
-          <div class='row'>
-            <div class='col-1-of-3'>
+          <div className='row'>
+            <div className='col-1-of-3'>
               Col 1 of 3
             </div>
-            <div class='col-2-of-3'>
+            <div className='col-2-of-3'>
               Col 2 of 3
             </div>
           </div>
 
-          <div class='row'>
-            <div class='col-1-of-4'>
+          <div className='row'>
+            <div className='col-1-of-4'>
               Col 1 of 4
             </div>
-            <div class='col-1-of-4'>
+            <div className='col-1-of-4'>
               Col 1 of 4
             </div>
-            <div class='col-1-of-4'>
+            <div className='col-1-of-4'>
               Col 1 of 4
             </div>
-            <div class='col-1-of-4'>
+            <div className='col-1-of-4'>
               Col 1 of 4
             </div>
           </div>
 
-          <div class='row'>
-            <div class='col-1-of-4'>
+          <div className='row'>
+            <div className='col-1-of-4'>
               Col 1 of 4
             </div>
-            <div class='col-1-of-4'>
+            <div className='col-1-of-4'>
               Col 1 of 4
             </div>
-            <div class='col-2-of-4'>
+            <div className='col-2-of-4'>
               Col 2 of 4
             </div>
           </div>
 
-          <div class='row'>
-            <div class='col-1-of-4'>
+          <div className='row'>
+            <div className='col-1-of-4'>
               Col 1 of 4
             </div>
-            <div class='col-3-of-4'>
+            <div className='col-3-of-4'>
               Col 3 of 4
             </div>
 
